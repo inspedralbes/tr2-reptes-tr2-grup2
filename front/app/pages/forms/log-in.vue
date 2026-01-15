e67e946dd8ff2c28494e896427b820f9632551f8
AlbaS-R <albasanchezromero345@gmail.com>
2026-01-15 08:33:31 +0100

    link login a signup

--- front/app/pages/forms/log-in.vue
+++ front/app/pages/forms/log-in.vue
@@ -3,9 +3,6 @@ import Encabezado from "@/layouts/encabezado.vue";
 
 const input_email = ref("");
 const input_pass = ref("");
-
-
-
 </script>
 <template>
   <Encabezado></Encabezado>
@@ -14,29 +11,38 @@ const input_pass = ref("");
       <div id="prim-menu">
         <h3>Indica les teves credencials:</h3>
         <br />
-        <br>
-        <input v-model="input_email" type="text" placeholder="Email" required>
-        <br>
-        <br>
-        <input v-model="input_pass" type="text" placeholder="Contraseña" required>
-        <br>
-        <br>
-        <a>He oblidat la meva contrasenya.</a>
-        <br>
-        <br>
-        <button> Entrar </button>
-        
+        <br />
+        <input v-model="input_email" type="text" placeholder="Email" required />
+        <br />
+        <br />
+        <input
+          v-model="input_pass"
+          type="text"
+          placeholder="Contraseña"
+          required
+        />
+        <br />
+        <br />
+        <a href="">He oblidat la meva contrasenya.</a>
+        <br />
+        <NuxtLink to="/forms/sign-in" custom v-slot="{ navigate }">
+          <a href="/forms/sign-in">No tens compte? Dona't d'alta!</a>
+        </NuxtLink>
+
+        <br />
+        <br />
+        <button>Entrar</button>
       </div>
     </div>
   </div>
 </template>
 <style scoped>
-    @font-face {
+@font-face {
   font-family: "Coolvetica";
   src: url(/assets/fuentes/coolvetica/Coolvetica\ Rg.otf);
 }
 #cuerpo {
-    font-family: "Coolvetica";
+  font-family: "Coolvetica";
   margin-top: 100px;
   display: flex;
   justify-content: center;
@@ -45,7 +51,7 @@ const input_pass = ref("");
 }
 #contenidor {
   justify-content: center;
-  font-weight:lighter; 
+  font-weight: lighter;
   text-align: center;
   background-color: #ffffff;
   -webkit-box-shadow: 0px 0px 13px 3px #909090;
@@ -56,33 +62,32 @@ const input_pass = ref("");
   min-height: 350px;
 }
 
-input{
-    width: 200px;
-    height: 30px;
-    background-color: #D0D0D0;
-    padding-left: 20px;
-    border-radius: 20px;
-    border-color: transparent;
-    font-size: large;
+input {
+  width: 200px;
+  height: 30px;
+  background-color: #d0d0d0;
+  padding-left: 20px;
+  border-radius: 20px;
+  border-color: transparent;
+  font-size: large;
 }
 
-a{
-    font-size: smaller;
+a {
+  font-size: smaller;
 }
 
-button{
-    width: 100px;
-    height: 50px;
-    font-size: large;
-    border-color: transparent;
-    border-radius: 15px;
-    background-color: #1F0DCA;
-    color: white;
-    transition: 0.3s;
-    cursor: pointer;
+button {
+  width: 100px;
+  height: 50px;
+  font-size: large;
+  border-color: transparent;
+  border-radius: 15px;
+  background-color: #1f0dca;
+  color: white;
+  transition: 0.3s;
+  cursor: pointer;
 }
-button:hover{
-    opacity: 0.8;
+button:hover {
+  opacity: 0.8;
 }
-
 </style>
