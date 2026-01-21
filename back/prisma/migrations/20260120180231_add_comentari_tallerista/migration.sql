b444a89229bd4ba815358debecdb8c537d09b9e3
Paula <a24pauvermac@inspedralbes.cat>
2026-01-21 21:16:55 +0100

    Translate CriterisWeights table to Catalan: criteri, pes, dataActualitzacio

--- back/prisma/migrations/20260120180231_add_comentari_tallerista/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE `Tallers` ADD COLUMN `comentari_profe` TEXT NULL,
-    ADD COLUMN `comentari_tallerista` VARCHAR(191) NULL,
-    ADD COLUMN `mailTallerista` VARCHAR(191) NULL;
