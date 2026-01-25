15635f69f2cb10161afa51b57f7c7f6489fd3717
a23cliferand <a23cliferand@inspedralbes.cat>
2026-01-25 01:53:59 +0100

    Remove authentication middleware for /talleristes route in nuxt.config.ts

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -14,6 +14,10 @@ export default defineNuxtConfig({
   routeRules: {
     "/admin/**": { appMiddleware: "auth" },
     "/centro/**": { appMiddleware: "auth" },
-    // "/talleristes/**": { appMiddleware: "auth" },
+  },
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+    },
   },
 });
