3bf1fd11c7baaad600f96462f9e52a8c15091b14
a23cliferand <a23cliferand@inspedralbes.cat>
2026-01-25 02:17:29 +0100

    Update meta description and enhance route rules in nuxt.config.ts

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -2,22 +2,28 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+
   app: {
     head: {
       meta: [
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
-        { name: "description", content: "La teva descripció aquí" },
+        { name: "description", content: "Descripción" },
       ],
     },
   },
+
   routeRules: {
-    "/admin/**": { appMiddleware: "auth" },
-    "/centro/**": { appMiddleware: "auth" },
+    "/admin/**": { appMiddleware: "auth", ssr: false },
+    "/centro/**": { appMiddleware: "auth", ssr: false },
+    // Aseguramos que estas rutas se traten como estáticas
+    "/talleristes/**": { prerender: true },
   },
+
   nitro: {
     prerender: {
       crawlLinks: true,
+      routes: Array.from({ length: 50 }, (_, i) => `/talleristes/${i + 1}`),
     },
   },
 });
